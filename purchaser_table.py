12915736cb249d8e199b60ed79ca8a22e9d77d9a
NapatDej <pat.dejaumnuypol@gmail.com>
2023-04-26 20:18:09 +0700

    upload resource

--- purchaser_table.py
+++ purchaser_table.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from typing import Literal
+from numpy.linalg import inv
 
 def load_data(file_name):
     raw_df = pd.read_excel(f'resource/{file_name}',skiprows=1)
@@ -12,6 +13,7 @@ def load_data(file_name):
     raw_df['cost'] = cost
     # df[['purchaser','wholesale','retail','transport','import']] = df[['purchaser','wholesale','retail','transport','import']]*1000
     
+
     return raw_df
 
 def etl(raw_df,target):
@@ -79,25 +81,33 @@ def validate(df,target):
     else:
         validate_status = f"xxxxx YOUR {target} TABLE IS UNVALIDATED xxxxx".upper()
 
+
     return validate_status
 
 def transform_to_io(df_purchaser):
     df_purchaser = df_purchaser.drop(['190','209','210'])
     df_purchaser = df_purchaser.drop(columns=['190','309','310','409','509','600','700'])
-    df_purchaser.to_excel('purchaser_2010.xlsx')
+
     df_a = df_purchaser.iloc[0:-4, 0:-13]
     df_value_add = df_purchaser.iloc[-4:, 0:-13]
-    df_final_demand = df_purchaser.iloc[0:-4, -13:-7]
     df_import = df_purchaser.iloc[0:-4, -7:-3].transpose()*-1
     df_indirect_tax = df_purchaser.iloc[0:-4, -3:].transpose()*-1
+    df_final_demand = df_purchaser.iloc[0:-4, -13:-7]
+
     df_io = pd.concat([df_a,df_value_add,df_import,df_indirect_tax])
     df_io = df_io.join(df_final_demand).fillna(0)
 
-    return df_io
 
+    return df_io, df_a, df_value_add, df_import, df_indirect_tax, df_final_demand
+
+
+def leontief():
+    a = 3
 
 if __name__ == '__main__':
-    raw_df = load_data(file_name = "2 -- 16 sectors (2010) -- Multiplier - Exercise.xlsx")
+
+    # Load data
+    raw_df = load_data(file_name = "DataIO2010x16.xlsx")
 
     # Create Purchaser Table
     df_purchaser,target = etl(raw_df = raw_df,target='purchaser')
@@ -109,6 +119,12 @@ if __name__ == '__main__':
     validate_status = validate(df = df_producer,target=target)
     print(f"\n\n\n {validate_status}")
 
-    # Transform to IO table
-    df_io = transform_to_io(df_purchaser)
-    df_producer.to_excel('producer.xlsx')
+    # Transform to IO table by purchaser
+    df_io, df_a, df_value_add, df_import, df_indirect_tax, df_final_demand = transform_to_io(df_purchaser)
+
+    identity_array = np.identity(len(df_a)) * 1
+    df_sum = pd.DataFrame(df_io.sum(axis = 1)[0:-11]).transpose()
+    for col in df_a.columns:
+        df_a[col] = df_a[col]/df_sum[col][0]
+    df_leontief = pd.DataFrame(inv(identity_array - df_a.to_numpy()),columns = df_a.columns,index = df_a.index)
+    a = 3
